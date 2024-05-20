2bcf8179f16b4a7bbbc12907bc349ea3dcfc58d8
Shubham Kushwaha <ansh.kush.2410@gmail.com>
2024-05-20 13:41:27 +0530

    Drag & Drop across the Campaigns

--- src/components/Campaign/AllCampaign.js
+++ src/components/Campaign/AllCampaign.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import { data } from "../../utils/data";
 import { useSelector } from "react-redux";
 import { selectFilters } from "../../utils/sortSlice";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const AllCampaign = () => {
   const [showSort, setShowSort] = useState(false);
   const filters = useSelector(selectFilters);
-  const [characters, updateCharacters] = useState(data);
+  const [activeTask, setActiveTask] = useState(null);
+  const [campaignData, setCampaignData] = useState(data); 
 
-  const filteredData = data.filter((item) => {
+  const filteredData = campaignData.filter((item) => {
     if (filters.johnDoe && item.associate === "John Doe") return true;
     if (filters.peterEngland && item.associate === "Peter England") return true;
     if (filters.henryPaul && item.associate === "Henry Paul") return true;
@@ -20,34 +20,47 @@ const AllCampaign = () => {
     setShowSort(!showSort);
   };
 
-  function handleOnDragEnd(result) {
-    if (!result.destination) return;
+  const onDrop = (index1, index2) => {
+    console.log(
+      `${activeTask} is going to place into ${index1} and at the position ${index2}`
+    );
 
-    const items = Array.from(characters);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
+    if (activeTask == null || activeTask === undefined) return;
 
-    updateCharacters(items);
-  }
+    const newData = JSON.parse(JSON.stringify(campaignData));
+
+    const [sourceCampaignIndex, sourceTaskIndex] = activeTask;
+    const sourceTask = newData[sourceCampaignIndex].folders.tasks[sourceTaskIndex];
+
+    newData[sourceCampaignIndex].folders.tasks.splice(sourceTaskIndex, 1);
+
+    newData[index1].folders.tasks.splice(index2, 0, sourceTask);
+
+    setCampaignData(newData);
+  };
+
+  const onDragStart = (index1, index2) => {
+    setActiveTask([index1, index2]);
+  };
 
   return (
     <div className="h-screen border-r">
       <div className="flex justify-between px-10 py-6">
         <div className="flex">
-          <i class="uil uil-comment-alt-lines mr-4 text-xl text-gray-400"></i>
+          <i className="uil uil-comment-alt-lines mr-4 text-xl text-gray-400"></i>
           <p className="text-xl font-semibold text-gray-700">All Campaign</p>
         </div>
         <div className="text-xl">
           <button onClick={handleSort}>
-            <i class="uil uil-scroll px-2 text-gray-400"></i>
+            <i className="uil uil-scroll px-2 text-gray-400"></i>
           </button>
-          <i class="uil uil-ellipsis-h px-2 text-gray-400"></i>
+          <i className="uil uil-ellipsis-h px-2 text-gray-400"></i>
         </div>
       </div>
       <div className="m-14 mt-10">
         <div className="bg-gray-100 p-2 px-5 rounded-lg flex justify-between">
           <div className="flex font-semibold">
-            <i class="uil uil-plus"></i>
+            <i className="uil uil-plus"></i>
             <p>Add New</p>
           </div>
           <div className="">
@@ -69,63 +82,43 @@ const AllCampaign = () => {
         </div>
       </div>
       <div className="">
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="campaigns">
-            {(provided) => (
-              <ul
-                className="campaigns"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {(filteredData.length > 0 ? filteredData : characters).map(
-                  (item, index) => (
-                    <Draggable
-                      key={item.campaignName}
-                      draggableId={item.campaignName}
-                      index={index}
+        <ul>
+          {(filteredData.length > 0 ? filteredData : campaignData).map(
+            (item, index1) => (
+              <li className="mx-14 my-5" key={item.associate}>
+                <div className="font-semibold my-2 text-gray-600 bg-gray-100 p-2 px-5 rounded-lg">
+                  <i className="uil uil-megaphone"></i> {item.campaignName}
+                </div>
+                <div className="text-gray-600 mx-6 py-1 border-b">
+                  <i className="uil uil-angle-right-b"></i>{" "}
+                  <i className="uil uil-folder-minus"></i>{" "}
+                  {item.folders.folderTitle}
+                </div>
+                <ul>
+                  {item.folders.tasks.map((task, index2) => (
+                    <li
+                      key={index2}
+                      className="flex justify-between text-gray-600 py-2 border-b mx-20 active:opacity-50 active:cursor-grab"
+                      draggable
+                      onDragStart={() => onDragStart(index1, index2)}
+                      onDragEnd={() => setActiveTask(null)}
+                      onDrop={() => onDrop(index1, index2)}
+                      onDragOver={(e) => e.preventDefault()}
                     >
-                      {(provided) => (
-                        <li
-                          className="mx-14 my-5"
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          ref={provided.innerRef}
-                        >
-                          <div className="font-semibold my-2 text-gray-600 bg-gray-100 p-2 px-5 rounded-lg">
-                            <i class="uil uil-megaphone"></i>{" "}
-                            {item.campaignName}
-                          </div>
-                          <div className="text-gray-600 mx-6 py-1 border-b">
-                            <i class="uil uil-angle-right-b"></i>{" "}
-                            <i class="uil uil-folder-minus"></i>{" "}
-                            {item.folders.folderTitle}
-                          </div>
-                          <div>
-                            {item.folders.tasks.map((item) => (
-                              <div className="flex justify-between text-gray-600 py-2 border-b mx-20">
-                                <div>
-                                  <i class="uil uil-clipboard-notes"></i>{" "}
-                                  {item.taskTitle}{" "}
-                                </div>
-                                <div className="text-xs text-blue-500">
-                                  <i class="uil uil-constructor"></i>{" "}
-                                  {item.status}{" "}
-                                  <i class="uil uil-calendar-slash"></i>{" "}
-                                  {item.dueDate}
-                                </div>
-                              </div>
-                            ))}
-                          </div>
-                        </li>
-                      )}
-                    </Draggable>
-                  )
-                )}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
+                      <div>
+                        <i className="uil uil-clipboard-notes"></i> {task.taskTitle}{" "}
+                      </div>
+                      <div className="text-xs text-blue-500">
+                        <i className="uil uil-constructor"></i> {task.status}{" "}
+                        <i className="uil uil-calendar-slash"></i> {task.dueDate}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            )
+          )}
+        </ul>
       </div>
     </div>
   );
--- src/components/Show.js
+++ src/components/Show.js
@@ -3,7 +3,7 @@ import { leaf_bg } from "../utils/constants";
 
 const Show = () => {
   return (
-    <div className=" h-screen">
+    <div className="">
       <img
       class="w-[25%] opacity-30 mx-auto mt-96"
         src={leaf_bg}
